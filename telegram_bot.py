f25498c35cddd54aece614b396c3e46c05c9f937
EvheniiR <jivtone@gmail.com>
2020-01-03 13:10:39 +0200

    bot updated

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,22 +1,62 @@
-import telebot
+import requests
 import config
+import json
+from time import sleep
+
+token = config.token
+URL = 'https://api.telegram.org/bot' + token + '/'
+
+global last_update_id
+last_update_id = 0
+
+def get_updates():
+    url  = URL + 'getupdates'
+    resp = requests.get(url)
+    return resp.json()
+
+def get_message():
+
+    data = get_updates()
+
+    last_object = data['result'][-1]
+    current_update_id = last_object['update_id']
+
+    global last_update_id
+    if last_update_id != current_update_id:
+        last_update_id = current_update_id        
+        chat_id = last_object['message']['chat']['id']
+        message_text = last_object['message']['text']
+
+        message = {
+        'chat_id' : chat_id,
+        'text' : message_text
+        }
+
+        return message
+    return None
+
+def send_message(chat_id, text='Жди, пидр, блять!'):
+    url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
+    requests.get(url)
 
 
-bot = telebot.TeleBot(config.token)
 
-#@bot.message_handler(commands=['start'])
-#def start_message(message):
-#   bot.send_message(message.chat.id, 'Пламенно зигую 0/0/0/!!!')
 
-@bot.message_handler(content_types=['text'])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
-#def send_text(message):
-    #if message.text.lower() == 'зигую':
-    #    bot.send_message(message.chat.id, 'Привет, мой создатель!')
-    #elif message.text.lower() == 'пока':
-    #    bot.send_message(message.chat.id, 'Прощай, создатель!')
+def main():
 
+    while True:        
+        answer = get_message()
+
+        if answer != None:
+            chat_id = answer['chat_id']
+            text = answer['text']
+
+            if 'ЖЕЛАЙ СПОКОЙНОЙ НОЧИ!' in text:
+                send_message(chat_id, 'Спокойной ночи , Кристина)!')
+        else:
+            continue
+
+        sleep(2)
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    main()
